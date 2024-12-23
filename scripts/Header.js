fba50fe636e7ce2efde611eb0a29d707025d6e98
wYeha <snovavzloman@mail.ru>
2024-12-23 14:57:25 +0300

    header + footer + hero sections done

---
+++ scripts/Header.js
@@ -0,0 +1,30 @@
+class Header {
+	selectors = {
+		root: '[data-js-header]',
+		overlay: '[data-js-header-overlay]',
+		burgerButton: '[data-js-header-burger-button]',
+	}
+	stateClasses = {
+		isActive: 'is-active',
+		isLock: 'is-lock',
+	}
+
+	constructor() {
+		this.rootElement = document.querySelector(this.selectors.root)
+		this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
+		this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton)
+		this.bindEvents()
+	}
+
+	onBurgerClick = () => {
+		this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
+		this.overlayElement.classList.toggle(this.stateClasses.isActive)
+		document.documentElement.classList.toggle(this.stateClasses.isLock)
+	}
+
+	bindEvents() {
+		this.burgerButtonElement.addEventListener('click', this.onBurgerClick)
+	}
+}
+
+export default Header
